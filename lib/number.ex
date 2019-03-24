ad4b03b6d2f0a3a748832e27ab7d29d90a317565
Adam Lancaster <lancaster28@hotmail.com>
2019-03-24 21:24:47 +0000

    implement equals

--- lib/number.ex
+++ lib/number.ex
@@ -1,12 +1,20 @@
 defmodule Number do
+  defstruct [:sign, :size]
+
   def positive(size) do
+    %Number{size: size, sign: :positive}
   end
 
   def negative(size) do
+    %Number{size: size, sign: :negative}
   end
 
-  def equal?(number_1, number_2) do
-    raise "Not implemented"
+  def equal?(number_1 = %{size: size_1, sign: sign_1}, number_2 = %{sign: sign_2, size: size_2}) do
+    if sign_1 == sign_2 do
+      size_1 == size_2
+    else
+      false
+    end
   end
 
   def add(number_1, number_2) do
