fadedaf41b8c232cfe99cf844be4a85f0b751f80
Adam Lancaster <lancaster28@hotmail.com>
2019-03-27 19:18:44 +0000

    implement addition

--- test/number_test.exs
+++ test/number_test.exs
@@ -38,27 +38,45 @@ defmodule NumberTest do
 
   describe "addition" do
     test "+10 + +10" do
-      assert Number.add(Number.positive(10), Number.positive(10)) == Number.positive(20)
+      assert Number.equal?(
+               Number.add(Number.positive(10), Number.positive(10)),
+               Number.positive(20)
+             )
     end
 
     test "-10 + -10" do
-      assert Number.add(Number.negative(10), Number.negative(10)) == Number.negative(20)
+      assert Number.equal?(
+               Number.add(Number.negative(10), Number.negative(10)),
+               Number.negative(20)
+             )
     end
 
     test "-10 + +15" do
-      assert Number.add(Number.negative(10), Number.positive(15)) == Number.positive(5)
+      assert Number.equal?(
+               Number.add(Number.negative(10), Number.positive(15)),
+               Number.positive(5)
+             )
     end
 
     test "+10 + -15" do
-      assert Number.add(Number.positive(10), Number.negative(15)) == Number.negative(5)
+      assert Number.equal?(
+               Number.add(Number.positive(10), Number.negative(15)),
+               Number.negative(5)
+             )
     end
 
     test "+15 + -10" do
-      assert Number.add(Number.positive(15), Number.negative(10)) == Number.positive(5)
+      assert Number.equal?(
+               Number.add(Number.positive(15), Number.negative(10)),
+               Number.positive(5)
+             )
     end
 
     test "-15 + +10" do
-      assert Number.add(Number.negative(15), Number.positive(10)) == Number.negative(5)
+      assert Number.equal?(
+               Number.add(Number.negative(15), Number.positive(10)),
+               Number.negative(5)
+             )
     end
 
     test "+ 1 + -1 = -1 + +1" do
@@ -71,35 +89,59 @@ defmodule NumberTest do
 
   describe "subtraction" do
     test "+15 - +10" do
-      assert Number.subtract(Number.positive(15), Number.positive(10)) == Number.positive(5)
+      assert Number.equal?(
+               Number.subtract(Number.positive(15), Number.positive(10)),
+               Number.positive(5)
+             )
     end
 
     test "+10 - +15" do
-      assert Number.subtract(Number.positive(10), Number.positive(15)) == Number.negative(5)
+      assert Number.equal?(
+               Number.subtract(Number.positive(10), Number.positive(15)),
+               Number.negative(5)
+             )
     end
 
     test "-5 - -10" do
-      assert Number.subtract(Number.negative(5), Number.negative(10)) == Number.positive(5)
+      assert Number.equal?(
+               Number.subtract(Number.negative(5), Number.negative(10)),
+               Number.positive(5)
+             )
     end
 
     test "-10 - -5" do
-      assert Number.subtract(Number.negative(10), Number.negative(5)) == Number.negative(5)
+      assert Number.equal?(
+               Number.subtract(Number.negative(10), Number.negative(5)),
+               Number.negative(5)
+             )
     end
 
     test "-10 - +15" do
-      assert Number.subtract(Number.negative(10), Number.positive(15)) == Number.negative(25)
+      assert Number.equal?(
+               Number.subtract(Number.negative(10), Number.positive(15)),
+               Number.negative(25)
+             )
     end
 
     test "+10 - -15" do
-      assert Number.subtract(Number.positive(10), Number.negative(15)) == Number.positive(25)
+      assert Number.equal?(
+               Number.subtract(Number.positive(10), Number.negative(15)),
+               Number.positive(25)
+             )
     end
 
     test "+15 - -10" do
-      assert Number.subtract(Number.positive(15), Number.negative(10)) == Number.positive(25)
+      assert Number.equal?(
+               Number.subtract(Number.positive(15), Number.negative(10)),
+               Number.positive(25)
+             )
     end
 
     test "-15 - +10" do
-      assert Number.subtract(Number.negative(15), Number.positive(10)) == Number.negative(25)
+      assert Number.equal?(
+               Number.subtract(Number.negative(15), Number.positive(10)),
+               Number.negative(25)
+             )
     end
 
     test "+1 - +1 = -1 - -1" do
@@ -112,19 +154,31 @@ defmodule NumberTest do
 
   describe "multiplication" do
     test "+2 × +3 = +6" do
-      assert Number.multiply(Number.positive(2), Number.positive(3)) == Number.positive(6)
+      assert Number.equal?(
+               Number.multiply(Number.positive(2), Number.positive(3)),
+               Number.positive(6)
+             )
     end
 
     test "-2 × -3 = +6" do
-      assert Number.multiply(Number.negative(2), Number.negative(3)) == Number.positive(6)
+      assert Number.equal?(
+               Number.multiply(Number.negative(2), Number.negative(3)),
+               Number.positive(6)
+             )
     end
 
     test "+2 × -3 = -6'" do
-      assert Number.multiply(Number.positive(2), Number.negative(3)) == Number.negative(6)
+      assert Number.equal?(
+               Number.multiply(Number.positive(2), Number.negative(3)),
+               Number.negative(6)
+             )
     end
 
     test "-2 × +3 = -6" do
-      assert Number.multiply(Number.negative(2), Number.positive(3)) == Number.negative(6)
+      assert Number.equal?(
+               Number.multiply(Number.negative(2), Number.positive(3)),
+               Number.negative(6)
+             )
     end
 
     test "+2 × +0 = -3 × +0" do
