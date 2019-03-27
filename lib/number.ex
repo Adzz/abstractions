5eb8b9014c2637c0d5906a4955e61ecee597affa
Adam Lancaster <lancaster28@hotmail.com>
2019-03-27 22:26:43 +0000

    implement less_than

--- lib/number.ex
+++ lib/number.ex
@@ -28,7 +28,7 @@ defmodule Number do
     }
   end
 
-  def less_than?(number_1, number_2) do
-    raise "Not implemented"
+  def less_than?(%{left: left, right: right}, %{left: other_left, right: other_right}) do
+    other_left + right < left + other_right
   end
 end
