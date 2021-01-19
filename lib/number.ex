ad520e34f351e106b28e341a23cdf2e5eacd7407
Adam Lancaster <lancaster28@hotmail.com>
2021-01-19 12:45:00 +0000

    Implements equality

--- lib/number.ex
+++ lib/number.ex
@@ -2,15 +2,15 @@ defmodule Number do
   defstruct [:sign, :size]
 
   def positive(size) do
-     %Number{size: size, sign: :positive}
+    %Number{size: size, sign: :positive}
   end
 
   def negative(size) do
     %Number{size: size, sign: :negative}
   end
 
-  def equal?(number_1, number_2) do
-    raise "Not implemented"
+  def equal?(%{size: size_1, sign: sign_1}, %{sign: sign_2, size: size_2}) do
+    sign_1 == sign_2 && size_1 == size_2
   end
 
   def add(number_1, number_2) do
